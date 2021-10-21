93abfd7a62a345d4e7b71cc68f00160eba4ccc59
kbu715 <kbu715@gmail.com>
2021-10-21 19:19:11 +0900

    ✨ mapStateToProps second parameter (ownProps)

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,11 +5,14 @@ import store from './redux/store'
 import HooksCakeContainer from './components/HooksCakeContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <HooksIceCreamContainer />
