cfda8d442cbadaacded825d66f66c6ef5ba0af15
kbu715 <kbu715@gmail.com>
2021-10-21 21:48:39 +0900

    ✨ Redux thunk get request in React Redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
         <ItemContainer cake />
         <ItemContainer />
         <HooksCakeContainer />
--- react-redux-demo/src/redux/user/userActions.js
+++ react-redux-demo/src/redux/user/userActions.js
@@ -1,4 +1,21 @@
+import axios from 'axios'
 import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from './userTypes'
+
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchUsersRequest())
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        // response.data is the users
+        const users = response.data
+        dispatch(fetchUsersSuccess(users))
+      })
+      .catch(error => {
+        // error.message is the error message
+        dispatch(fetchUsersFailure(error.message))
+      })
+  }
+}
 
 export const fetchUsersRequest = () => {
   return {
