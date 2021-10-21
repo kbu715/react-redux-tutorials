7d5914b9f3fbc1878a3ee3eac485d4fcaca1f280
kbu715 <kbu715@gmail.com>
2021-10-21 16:12:57 +0900

    ✨ Implement useSelector and useDispatch hooks and multiple reducers in React Redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import HooksCakeContainer from './components/HooksCakeContainer';
+import HooksIceCreamContainer from './components/HooksIceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
+        <HooksIceCreamContainer />
       </div>
     </Provider>
   );
